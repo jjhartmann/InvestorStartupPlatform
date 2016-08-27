ce99a52d4f1db2f690f4a1ffdf5525c5a44b1591
Jeremy J Hartmann <jhartman@sfu.ca>
2016-08-26 22:46:50 -0400

    Add EnterpriseUser model

---
+++ app/models/enterprise_user.rb
@@ -0,0 +1,2 @@
+class EnterpriseUser < ApplicationRecord
+end
---
+++ test/models/enterprise_user_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class EnterpriseUserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
