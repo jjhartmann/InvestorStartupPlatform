376534409f3e80fa64a1cb50557db1e6916e32fe
Jeremy J Hartmann <jhartman@sfu.ca>
2016-08-26 23:06:53 -0400

    Add Enterprise model

---
+++ db/migrate/20160827030438_create_enterprises.rb
@@ -0,0 +1,26 @@
+class CreateEnterprises < ActiveRecord::Migration[5.0]
+  def change
+    create_table :enterprises do |t|
+      t.string :name
+      t.text :pitch
+      t.integer :funds_to_raise
+      t.string :industry
+      t.string :enterprise_stage
+      t.string :stage_identifier
+      t.string :market_identifier
+      t.string :location
+      t.text :description
+      t.string :logo
+      t.integer :follower_count, default: 0
+      t.integer :followed_count, default: 0
+      t.integer :comment_count, default: 0
+
+      t.timestamps default: Time.now
+    end
+
+    add_index :enterprises, :name, unique: true
+    add_index :enterprises, :location
+    add_index :enterprises, [:market_identifier, :location]
+
+  end
+end
