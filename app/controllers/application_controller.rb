35cee1ea7715ac5b57c67601dfa4a289ed3eabfb
Jeremy J Hartmann <jhartman@sfu.ca>
2016-08-31 22:39:02 -0400

    Add authorization through cancancan
    - Add exception handling in ApplicationController.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,22 @@ class ApplicationController < ActionController::Base
 
   protected
 
+  rescue_from CanCan::AccessDenied do |exception|
+    if current_user.nil?
+      session[:next] = request.fullpath
+      puts session[:next]
+      redirect_to login_url, :alert => "You have to log in to continue."
+    else
+      #render :file => "#{Rails.root}/public/403.html", :status => 403
+      if request.env["HTTP_REFERER"].present?
+        redirect_to :back, :alert => exception.message
+      else
+        redirect_to main_app.root_url, :alert => exception.message
+      end
+    end
+  end
+
+
   def configure_permitted_parameters
     added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
