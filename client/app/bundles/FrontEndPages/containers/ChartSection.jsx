e40c57f31b561dc1a4a232c1c8cde37773ee5f59
Jeremy J Hartmann <jhartman@sfu.ca>
2016-09-01 18:14:59 +0530

    Integrate Chart with Services; Recraete Services with react components; Include Stylesheets with Webpack

---
+++ client/app/bundles/FrontEndPages/containers/ChartSection.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactOnRails from 'react-on-rails'
+import HeaderText from '../components/SectionHeader'
+import ChartWidget from '../components/ChartComponent'
+
+export default class ChartSection extends React.Component {
+  render() {
+    return (
+    <section className="modify">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <HeaderText about_text="Charts" />
+            <ChartWidget></ChartWidget>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+}
