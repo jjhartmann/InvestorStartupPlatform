3eba79c98e22cbf373582bf5bb0c94fba59048af
Jeremy J Hartmann <jhartman@sfu.ca>
2016-08-31 23:41:00 -0400

    Update question and questionaire models and fix rails_admin interface
    - fix errors with namespacing kaminari
    - add admin to prime user in dev_seeds.

--- app/models/quesiton.rb
+++
@@ -1,2 +0,0 @@
-class Quesiton < ApplicationRecord
-end
---
+++ db/migrate/20160827023955_create_questions.rb
@@ -0,0 +1,15 @@
+class CreateQuestions < ActiveRecord::Migration[5.0]
+  def change
+    create_table :questions do |t|
+      t.text :question
+      t.text :answer
+      t.string :category
+      t.integer :questionaire_id
+
+      t.timestamps default: Time.now
+    end
+
+    add_index :questions, :questionaire_id
+    add_index :questions, :category
+  end
+end
