74f596cebec18e6d0e3105d670b196f66ed8128f
Jeremy J Hartmann <jhartman@sfu.ca>
2016-09-04 15:56:54 -0400

    Add questions to ask investors model/migration

---
+++ db/migrate/20160904185604_create_questions_to_ask_investors.rb
@@ -0,0 +1,11 @@
+class CreateQuestionsToAskInvestors < ActiveRecord::Migration[5.0]
+  def change
+    create_table :questions_to_ask_investors do |t|
+      t.string :question
+      t.string :qtype
+      t.integer :position
+
+      t.timestamps
+    end
+  end
+end
