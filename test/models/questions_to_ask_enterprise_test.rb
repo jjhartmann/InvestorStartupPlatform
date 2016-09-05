e2a5e7a191c92707670a9e7b85f387753681d8c0
Jeremy J Hartmann <jhartman@sfu.ca>
2016-09-04 23:16:12 -0400

    Add migrations for questions to ask entrepreneurs and enterprises

---
+++ test/models/questions_to_ask_enterprise_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class QuestionsToAskEnterpriseTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
---
+++ test/models/questions_to_ask_entrepreneur_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class QuestionsToAskEntrepreneurTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
