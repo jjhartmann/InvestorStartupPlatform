d9fc7606a753a1496a5dab1fac1ccd00679fcde8
Shantilal <shm.narola@gmail.com>
2016-09-05 18:31:17 +0530

    Login and Registration; Recreate the Login components

--- client/app/bundles/FrontEndPages/startup/Login.jsx
+++ client/app/bundles/FrontEndPages/startup/Login.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import ReactOnRails from 'react-on-rails'
 import LoginPage from '../containers/LoginPage'
-
+import RegisterHeader from '../components/RegisterHeader'
 
 const Login = (props, _railsContext) => {
   const reactComponent = (
-    <div>
-      <LoginPage />
+    <div className="login-set">
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12">
+            <div className="log-in box">
+              <RegisterHeader header={["Login"]}/>
+              <LoginPage {...props} />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
   return reactComponent;
