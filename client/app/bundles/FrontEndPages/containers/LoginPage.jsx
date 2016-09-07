3087146d1a4f54fab6fa4f4bc0b724c40600b30b
Jeremy J Hartmann <jhartman@sfu.ca>
2016-09-07 14:14:02 +0530

    Login with username and email

--- client/app/bundles/FrontEndPages/containers/LoginPage.jsx
+++ client/app/bundles/FrontEndPages/containers/LoginPage.jsx
@@ -9,7 +9,7 @@ export default class LoginPage extends React.Component {
     return (
         <form role='form' accept-charset="UTF-8" action='/users/sign_in' method='post'>
           <input type='hidden' name='authenticity_token' value={this.props.data.authenticity_token} />
-          <TextField placeholder="Enter UserName/Email" type="text" name="user[email]"></TextField>
+          <TextField placeholder="Enter UserName/Email" type="text" name="user[login]"></TextField>
           <TextField placeholder="Enter your Password" type="password" name="user[password]"></TextField>
           <LoginForgetPassword />
           <SubmitButton />
