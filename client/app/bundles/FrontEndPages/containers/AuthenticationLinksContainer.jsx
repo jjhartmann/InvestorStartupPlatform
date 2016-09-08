ed41ee86a246396aa4bb6be3c145c272ba0b3761
Shanti Lal <shm.narola@gmail.com>
2016-09-08 15:15:46 +0530

    Authentication Link Changes

--- client/app/bundles/FrontEndPages/containers/AuthenticationLinksContainer.jsx
+++ client/app/bundles/FrontEndPages/containers/AuthenticationLinksContainer.jsx
@@ -5,31 +5,31 @@ import auth from '../containers/auth'
 
 // Simple example of a React "smart" component
 export default class AuthenticationLink extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {loggedIn: auth.loggedIn()}
-  }
 
-  updateAuth(loggedIn) {
-    this.setState({
-      loggedIn
-    })
-  }
+    constructor(props)
+    {
+      super(props);
+      this.state = { loggedIn: "ioj" }
+    }
 
-  componentWillMount() {
-    auth.onChange = this.updateAuth
-    auth.login()
-  }
+    updateAuth(loggedIn) {
+      this.setState({
+        loggedIn
+      })
+    }
 
+    componentWillMount() {
+
+    }
   render() {
     return (
     <div>
-        <ul className="nav navbar-nav navbar-right">
-          {this.state.loggedIn ?(
+        <ul className="nav navbar-nav navbar-right">          
+          {this.state.loggedIn ? (
             <LinkWidget name="Login" path="/users/sign_in"/>
-            ):(
+            ) : (
               <LinkWidget name="Logout" path="/"/>
-              )
+            )
           }
             <LinkWidget name="Join" path="/users/sign_up"/>
         </ul>
