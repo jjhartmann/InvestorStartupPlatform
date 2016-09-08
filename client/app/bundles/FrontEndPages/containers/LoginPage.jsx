ddbf56905b0a07dd26532ab00126b6ff5ed06f96
Shantilal <shm.narola@gmail.com>
2016-09-08 11:19:25 +0530

    Change Input in RegisterContainer and LoginPage

--- client/app/bundles/FrontEndPages/containers/LoginPage.jsx
+++ client/app/bundles/FrontEndPages/containers/LoginPage.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import ReactOnRails from 'react-on-rails'
 import LoginForgetPassword from '../components/LoginForgetPassword'
 import TextField from '../components/TextField'
+import RegisterHeader from '../components/RegisterHeader'
 import SubmitButton from '../components/SubmitButton'
 
 export default class LoginPage extends React.Component {
   render() {
     return (
         <form role='form' acceptCharset="UTF-8" action='/users/sign_in' method='post'>
+          <RegisterHeader header={["Login"]}/>
           <input type='hidden' name='authenticity_token' value={this.props.data.authenticity_token} />
-          <TextField placeholder="Enter UserName/Email" type="text" name="user[login]"></TextField>
-          <TextField placeholder="Enter your Password" type="password" name="user[password]"></TextField>
+          <input placeholder="Username" type="text" required="" name="user[login]"/>
+          <input placeholder="Password" type="password" required="" name="user[password]" />
           <LoginForgetPassword />
           <SubmitButton />
         </form>
