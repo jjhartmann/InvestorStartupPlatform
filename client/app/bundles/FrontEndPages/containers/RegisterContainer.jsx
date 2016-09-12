4f7e214e4d1e0ec0dd5dc35eb4f8fd103cce1b9b
Shantilal <shm.narola@gmail.com>
2016-09-12 09:30:28 +0530

    Errors in Registration

--- client/app/bundles/FrontEndPages/containers/RegisterContainer.jsx
+++ client/app/bundles/FrontEndPages/containers/RegisterContainer.jsx
@@ -1,7 +1,10 @@
 import React, { PropTypes } from 'react'
+import ReactDOM from 'react-dom';
 import SubmitButton from '../components/SubmitButton'
 import RegisterHeader from '../components/RegisterHeader'
-
+import Error from '../components/Error'
+import RegisterationLinks from '../components/RegisterableLink'
+import TransitionGroup from 'react-addons-transition-group';
 
 export default class Register extends React.Component {
   // Get Initial stae in ES5
@@ -14,7 +17,8 @@ export default class Register extends React.Component {
                      password: '',
                      password_confirmation: ''
                    },
-                   errors: {}
+                   errors: {},
+                   items: []
                 };
   }
 
@@ -49,20 +53,6 @@ export default class Register extends React.Component {
     this.setState({user: newUser});
   }
 
-  show_error1 = (errors) => {
-      alert("yo");
-      error = errors;
-      console.log(error);
-  }
-
-  show_error = (error) => {
-      alert("yo");
-      for (const e2 in error) {
-          console.log( e2 + '->' + error[e2] );
-      }
-  }
-
-  // Form Submit
   submit(e){
     e.preventDefault();
     var data = {
@@ -76,7 +66,6 @@ export default class Register extends React.Component {
         data: data,
         dataType: "json",
         success: (data) => {
-          alert("registered.");
           console.log(data);
           this.setState({
             user: {
@@ -88,40 +77,101 @@ export default class Register extends React.Component {
             },
             errors: {}
           });
+          location.reload();
         },
         error: (data) => {
-          // this.show_error({errors: data.responseJSON.errors})
           this.setState({errors: data.responseJSON.errors})
-          // console.log(data.responseJSON.errors);
+          this.show_error()
         }
       });
   }
+  show_error = () => {
+    this.state.items = []
+    var pairs = this.state.items
+    if (this.state.errors.name != null){
+      this.state.errors.name.map(function(player){
+        pairs.push("Name " + player);
+      })
+    }
+    if (this.state.errors.email != null){
+      this.state.errors.email.map(function(player){
+        pairs.push("Email " + player);
+      })
+    }
+    if (this.state.errors.username != null){
+      this.state.errors.username.map(function(player){
+        pairs.push("User Name " + player);
+      })
+    }
+    if (this.state.errors.password != null){
+      this.state.errors.password.map(function(player){
+        pairs.push("Password " + player);
+      })
+    }
+    if (this.state.errors.password_confirmation != null){
+      this.state.errors.password_confirmation.map(function(player){
+        pairs.push("Confirm Password " + player);
+      })
+    }
+    this.setState({items: pairs})
+    console.log(this.state.items);
+  }
+
+
+// Form Submit
   render() {
+    var errors = this.state.items.map(function(item){
+      return (
+        <Error error={item}></Error>
+      );
+    });
     return (
-      <form onSubmit={this.submit.bind(this)}>
-        <RegisterHeader header={["Register Now!!"]}/>
-        <div className="form-group">
-          <input type="text" placeholder="Enter your full name" value={this.state.user.name} onChange={this.updateName.bind(this)} />
-          <span style={{color: 'red'}}>{this.state.errors.name}</span>
+      <div>
+        <div id="floating_alert">
+          {errors}
         </div>
-        <div className="form-group">
-          <input type="text" placeholder="Enter your Email-Address" value={this.state.user.email} onChange={this.updateEmail.bind(this)} />
-          <span style={{color: 'red'}}>{this.state.errors.email}</span>
+        <div className="registration col-xs-12 col-sm-6">
+          <div className="">
+            <form onSubmit={this.submit.bind(this)}>
+              <RegisterHeader header={["Register Now!!"]}/>
+              <div className="form-group">
+                <input type="text" placeholder="Enter your full name" value={this.state.user.name} onChange={this.updateName.bind(this)} />
+              </div>
+              <div className="form-group">
+                <input type="text" placeholder="Enter your Email-Address" value={this.state.user.email} onChange={this.updateEmail.bind(this)} />
+              </div>
+              <div className="form-group">
+                <input type="text" placeholder="Enter your desired user name" value={this.state.user.username} onChange={this.updateUserName.bind(this)} />
+              </div>
+              <div className="form-group">
+                <input type="password" placeholder="Password" value={this.state.user.password} onChange={this.updatePassword.bind(this)} />
+              </div>
+              <div className="form-group">
+                <input type="password" placeholder="Confirm your Password" value={this.state.user.password_confirmation} onChange={this.updateConfirmPassword.bind(this)} />
+              </div>
+              <SubmitButton />
+            </form>
+          </div>
         </div>
-        <div className="form-group">
-          <input type="text" placeholder="Enter your desired user name" value={this.state.user.username} onChange={this.updateUserName.bind(this)} />
-          <span style={{color: 'red'}}>{this.state.errors.username}</span>
+        <div className="registration col-xs-12 col-sm-6">
+          <div className="">
+            <div className="clearfix">
+              <div className="links">
+                <RegisterHeader header={["Join Us with"]}/>
+                <ul className="social">
+                  <RegisterationLinks link_name="Wee Chat" key="we-chat"/>
+                  <li><h6>OR</h6></li>
+                  <RegisterationLinks link_name="QQ" key="qq"/>
+                  <li><h6>OR</h6></li>
+                  <RegisterationLinks link_name="Gmail" key="gmail"/>
+                  <li><h6>OR</h6></li>
+                  <RegisterationLinks link_name="Facebook" key="fb"/>
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className="form-group">
-          <input type="password" placeholder="Password" value={this.state.user.password} onChange={this.updatePassword.bind(this)} />
-          <span style={{color: 'red'}}>{this.state.errors.password}</span>
-        </div>
-        <div className="form-group">
-          <input type="password" placeholder="Confirm your Password" value={this.state.user.password_confirmation} onChange={this.updateConfirmPassword.bind(this)} />
-          <span style={{color: 'red'}}>{this.state.errors.password_confirmation}</span>
-        </div>
-        <SubmitButton />
-      </form>
+      </div>
     );
   }
 }
