87d96f0fdf8ecf3ddba162d1e2e0c56e0ec27d3a
Jeremy J Hartmann <jhartman@sfu.ca>
2016-09-16 10:50:46 +0530

    Authenticate user for Questionaire

--- app/controllers/questionaries_controller.rb
+++ app/controllers/questionaries_controller.rb
@@ -1,16 +1,14 @@
 class QuestionariesController < ApplicationController
   layout 'frontpage'
+  before_filter :authenticate_user!
+  
   def index
-    puts current_user.profilable_type
     @user_type = current_user.profilable_type
     if @user_type == "UserProfile"
-      @questions = QuestionsToAskEntrepreneur.all
-      puts "____________"
+      @questions = QuestionsToAskEntrepreneur.all.as_json
     elsif @user_type == "InvestorProfile"
-      @questions = QuestionsToAskInvestor.all
-      puts "**************"
+      @questions = QuestionsToAskInvestor.all.as_json
     end
-    puts @questions.as_json
   end
 
   def save_questions
