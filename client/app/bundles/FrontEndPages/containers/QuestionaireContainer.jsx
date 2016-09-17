d1ca835a747dc1019187c1e48bde13f6cb2b5951
Jeremy J Hartmann <jhartman@sfu.ca>
2016-09-17 12:41:48 +0530

    Prohibiting to change id in url

--- client/app/bundles/FrontEndPages/containers/QuestionaireContainer.jsx
+++ client/app/bundles/FrontEndPages/containers/QuestionaireContainer.jsx
@@ -18,7 +18,7 @@ export default class QuestionaireContainer extends React.Component {
       type: "GET",
       url: "/questionaries/save_questions",
       success: ()=>{
-        alert("hello")
+        alert("hello");
       }
     })
   }
