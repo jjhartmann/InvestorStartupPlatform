0da2fb101c32269c1a4e0f2ec121b8bbe30b9cc5
Shanti Lal <shm.narola@gmail.com>
2016-09-17 12:41:08 +0530

    Prohibiting to change id in url

---
+++ client/app/bundles/FrontEndPages/containers/ProfilePage.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactOnRails from 'react-on-rails'
+import SidebarTab from '../components/SidebarTabs'
+import Header from '../components/Header'
+import ProfileDetails from '../components/ProfileDetails'
+
+export default class ProfilePage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = { profile_details: this.props.data,
+                   profilable: this.props.profilable,
+                 };
+  }
+  render() {
+    return (
+      <div>
+        <SidebarTab />
+        <ProfileDetails {...this.props} />
+     </div>
+
+
+  );
+}
+}
