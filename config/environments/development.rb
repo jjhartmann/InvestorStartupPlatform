a8cbc78c22e5a4f25decb60c6c7aa26f3d3d4483
Shantilal <shm.narola@gmail.com>
2016-09-19 18:42:17 +0530

    Change Default Locale in config/environments/development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   #     format:               :html
   # }
 
-  # config.i18n.default_locale = :'en'
+  config.i18n.default_locale = :'en'
 
-  config.i18n.default_locale = :'zh-CN' #for Chinese
+  # config.i18n.default_locale = :'zh-CN' #for Chinese
 end
