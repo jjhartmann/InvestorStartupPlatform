1e7ae3ead26375b53312b7cbcce57ea1af0b7b5b
Jeremy J Hartmann <jhartman@sfu.ca>
2016-09-19 18:40:14 +0530

    Create Notifications and Display in the Dashboard

---
+++ db/migrate/20160919120825_create_notifications.rb
@@ -0,0 +1,12 @@
+class CreateNotifications < ActiveRecord::Migration[5.0]
+  def change
+    create_table :notifications do |t|
+      t.integer :notificable_id
+      t.string :notificable_type
+      t.string :notification_text
+      t.string :is_viewed
+
+      t.timestamps
+    end
+  end
+end
