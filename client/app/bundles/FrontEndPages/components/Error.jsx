a68154660b59f918522ad58feb05c651b1a91ab9
Shanti Lal <shm.narola@gmail.com>
2016-09-20 15:56:09 +0530

    Dismissal of alerts

--- client/app/bundles/FrontEndPages/components/Error.jsx
+++ client/app/bundles/FrontEndPages/components/Error.jsx
@@ -9,9 +9,10 @@ export default class Error extends React.Component {
   {
     const { error } = this.props;
     return (
-     <div className="alert alert-danger">
-      {error}
-    </div>
+    <div className="alert alert-danger">
+        {error}
+        <a href="#" className="alert-danger" style={{float: 'right'}} data-dismiss="alert" aria-label="close">&times;</a>
+      </div>
     );
   }
 }
