780cea36f6855d51ca3f117e5d81e6612eee290f
Jeremy J Hartmann <jhartman@sfu.ca>
2016-09-20 12:47:33 +0530

    get_notifications only when user is signed in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-
-  before_action :get_notifications
+  before_action :get_notifications, if: :user_signed_in?
 
   protected
 
