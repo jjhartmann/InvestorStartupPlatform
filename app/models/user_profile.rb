d23bb084fae95cf88f2af2a5600632108b52d710
Shanti Lal <shm.narola@gmail.com>
2016-10-03 18:08:11 +0530

    Sending the request for the meeting

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -5,6 +5,7 @@ class UserProfile < ApplicationRecord
   has_one :user, :as => :profilable
   has_one :questionaire, :as => :questionable
   has_many :notifications, :as => :notificable
+  has_one :meeting_member, :as => :memberable
 
   validates :profession,        :presence     => true,
             :length       => { :within => 1..140 }, on: :update
