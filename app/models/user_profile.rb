23898c2407921e7b0ed994924a407f51f7bda952
Shanti Lal <shm.narola@gmail.com>
2016-10-04 15:59:12 +0530

    Concern for the receicved time of messages and notification

--- app/models/user_profile.rb
+++ app/models/user_profile.rb
@@ -5,7 +5,7 @@ class UserProfile < ApplicationRecord
   has_one :user, :as => :profilable
   has_one :questionaire, :as => :questionable
   has_many :notifications, :as => :notificable
-  has_one :meeting_member, :as => :memberable
+  has_many :meeting_members, :as => :memberable
 
   validates :profession,        :presence     => true,
             :length       => { :within => 1..140 }, on: :update
