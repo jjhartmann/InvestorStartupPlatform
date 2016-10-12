1a56d226c7dabbe701f2c70daf70b74e16f2f354
Shantilal <shm.narola@gmail.com>
2016-10-12 10:23:25 +0530

    User in Meeting; InvestorProfile Association in Meeting

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,6 +1,6 @@
 class Meeting < ApplicationRecord
   belongs_to :enterprise
-  belongs_to :user
+  belongs_to :investor_profile
   has_many :meeting_members
   has_many :notifications, :as => :notificable
   has_many :meeting_rooms
--- db/migrate/20161011123841_remove_user_from_meeting.rb
+++
@@ -1,5 +0,0 @@
-class RemoveUserFromMeeting < ActiveRecord::Migration[5.0]
-  def change
-    remove_reference :meetings, :user
-  end
-end
