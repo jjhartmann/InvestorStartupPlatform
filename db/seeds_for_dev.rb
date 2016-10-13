332c96433aaf4a8d4f33d7d80401f372f8811c05
Shanti Lal <shm.narola@gmail.com>
2016-10-13 16:15:09 +0530

    Restricting Investor to schedule meeting according to time provided by Admin

--- db/seeds_for_dev.rb
+++ db/seeds_for_dev.rb
@@ -118,7 +118,7 @@ p ' > Admin Meeting Schedule'
 
 User.where(profilable_type: "InvestorProfile").each do |a|
  2.times do
-   @meeting = AdminMeetingSchedule.create!(user_id: a.id,day: Date::DAYNAMES.sample,start_time: (Time.now.beginning_of_day + rand(8..18).hour))
+   @meeting = AdminMeetingSchedule.create!(investor_profile_id: a.profilable_id,day: Date::DAYNAMES.sample,start_time: (Time.now.beginning_of_day + rand(8..18).hour))
  end
 end
 
