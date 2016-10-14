9c1253eb56e363e85a9ca236b5bdbcf7c9509d6e
Shanti Lal <shm.narola@gmail.com>
2016-10-14 12:27:47 +0530

    Change in notification dispaly for meeting

--- app/models/concerns/rails_admin/meeting_admin.rb
+++ app/models/concerns/rails_admin/meeting_admin.rb
@@ -4,10 +4,19 @@ module RailsAdmin::MeetingAdmin
   included do
     rails_admin do
       navigation_label 'Navigation'
-      navigation_icon 'fa fa-calendar'
-      label_plural 'MeetingsSchedules'
+      navigation_icon 'fa fa-users'
+      label_plural 'Meetings'
 
       create do
+        field :topic
+        field :start_time
+        field :end_time
+        field :enterprise_id
+        field :acceptance_status , :enum do
+          enum do
+            MeetingMember::ACCEPT_STATUS
+          end
+        end
       end
 
       edit do
