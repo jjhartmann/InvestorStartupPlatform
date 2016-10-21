e9fe7c68c6b84f425cf2d848b2155e02e5cf7737
Jeremy J Hartmann <jhartman@sfu.ca>
2016-10-21 15:18:50 +0530

    Design Changes in Profile

--- client/javascripts/all_users.js
+++ client/javascripts/all_users.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
       success: function(){
         // console.log("success");
         // console.log(link.text());
-        console.log(link);
+        console.log(link.text());
         console.log($('#user_'+link.data('id')).find(".tag-3 li.follow").text());
         if (link.text().trim() == "Follow"){
           link.text("Following");
