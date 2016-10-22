fdf4c16c98c4ecac8ad89fae9ec1a82955bdca43
Jeremy J Hartmann <jhartman@sfu.ca>
2016-10-22 13:19:41 +0530

    Login Page Implementation

--- client/app/bundles/FrontEndPages/components/LoginForgetPassword.jsx
+++ client/app/bundles/FrontEndPages/components/LoginForgetPassword.jsx
@@ -3,8 +3,9 @@ import React, { PropTypes } from 'react';
 export default class LoginPassword extends React.Component{
   render() {
     return (
-      <div className="form-group">
-        <a className="password-go" href="/users/password/new">{this.props.forgot_password}</a>
+      <div className="checkbox">
+        <label><input type="checkbox" /> Remember me</label>
+        <div className="forgot"><a href="#">Forgot Password</a></div>
       </div>
     );
   }
