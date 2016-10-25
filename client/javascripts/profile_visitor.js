46de02f5c1b073985aec4854c19799ae948f8095
Jeremy J Hartmann <jhartman@sfu.ca>
2016-10-25 14:12:35 +0530

    Js file for profile_visitor

---
+++ client/javascripts/profile_visitor.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $(".profile_visitor_follow").click(function(){
+    var link = $(this);
+    $.ajax({
+      url: "/user_dashboards/follow_unfollow_user",
+      type: "get",
+      data: {target_id: $(this).data('id'), target_type: $(this).data('type')},
+      success: function(){
+        if (link.text() == "Follow"){
+          link.text("Unfollow");
+        }else{
+          link.text("Follow");
+        }
+      }
+    })
+  });
+})
