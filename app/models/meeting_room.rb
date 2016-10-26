35582586f1d14a7bacd77d4285f5d353e94ddd39
Jeremy J Hartmann <jhartman@sfu.ca>
2016-10-26 12:12:54 +0530

    Function to enable and disble textbox in meeting room.

--- app/models/meeting_room.rb
+++ app/models/meeting_room.rb
@@ -2,4 +2,9 @@ class MeetingRoom < ApplicationRecord
   belongs_to :meeting
   has_many :meeting_room_members, dependent: :destroy
   has_many :meeting_room_messages, dependent: :destroy
+
+
+  def time_range
+    return meeting.start_time < DateTime.now && meeting.end_time > DateTime.now
+  end
 end
