7801613d14f66a68e8ae2c5c400de207d43ce992
Jeremy J Hartmann <jhartman@sfu.ca>
2016-10-26 16:30:11 +0530

    Conflict resolve; Meeting Requests and Enterprise invitations; Redesign Forgot password and Register page

--- client/app/bundles/FrontEndPages/components/LoginForgetPassword.jsx
+++ client/app/bundles/FrontEndPages/components/LoginForgetPassword.jsx
@@ -3,9 +3,8 @@ import React, { PropTypes } from 'react';
 export default class LoginPassword extends React.Component{
   render() {
     return (
-      <div className="checkbox">
-        <label><input type="checkbox" /> Remember me</label>
-        <div className="forgot"><a href="/users/password/new">Forgot Password</a></div>
+      <div className="">
+        <label><a href="/users/password/new">Forgot Password</a></label>
       </div>
     );
   }
