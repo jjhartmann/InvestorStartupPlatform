577f0883f9748b862e9f29b5284a34d95df8932a
Jeremy J Hartmann <jhartman@sfu.ca>
2016-11-06 14:45:56 -0500

    Fix for PG boolean conversion to binary

--- db/migrate/20161017113050_add_iv_and_key_to_meeting_room_message_document.rb
+++ db/migrate/20161017113050_add_iv_and_key_to_meeting_room_message_document.rb
@@ -1,6 +1,6 @@
 class AddIvAndKeyToMeetingRoomMessageDocument < ActiveRecord::Migration[5.0]
   def change
-    add_column :meeting_room_message_documents, :iv, :boolean
-    add_column :meeting_room_message_documents, :key, :boolean
+    add_column :meeting_room_message_documents, :iv, :binary
+    add_column :meeting_room_message_documents, :key, :binary
   end
 end
--- db/migrate/20161019050838_change_data_type_in_meeting_room_message_document.rb
+++ db/migrate/20161019050838_change_data_type_in_meeting_room_message_document.rb
@@ -1,11 +1,6 @@
 class ChangeDataTypeInMeetingRoomMessageDocument < ActiveRecord::Migration[5.0]
   def up
-    change_column :meeting_room_message_documents, :iv, 'bytea USING CAST(iv AS bytea)'
-    change_column :meeting_room_message_documents, :key, 'boolean USING CAST(key AS bytea)'
-
   end
   def down
-    change_column :meeting_room_message_documents, :iv, :boolean
-    change_column :meeting_room_message_documents, :key, :boolean
   end
 end
