43ffce2ba48f0289f50b527f94331ac1cb16806a
Jeremy J Hartmann <jhartman@sfu.ca>
2016-11-06 14:15:15 -0500

    Change column for boolean cast

--- db/migrate/20160920045833_change_data_type_in_notification.rb
+++ db/migrate/20160920045833_change_data_type_in_notification.rb
@@ -1,6 +1,6 @@
 class ChangeDataTypeInNotification < ActiveRecord::Migration[5.0]
   def up
-    change_column :notifications, :is_viewed, :boolean
+    change_column :notifications, :is_viewed, 'boolean USING CAST(is_viewed AS boolean)'
   end
 
   def down
