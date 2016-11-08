090fa7fc9a391ad4f784d30a5c678b3626a1bf62
Jeremy J Hartmann <jhartman@sfu.ca>
2016-11-08 00:54:58 -0500

    Update default action mailer options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   #TODO change host and port to conform to AWS site
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'netpluswebapp.herokuapp.com'}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
